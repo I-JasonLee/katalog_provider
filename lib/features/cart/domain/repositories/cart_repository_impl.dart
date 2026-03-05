c2d4b322950f4d5b915bbbc49bb1dc629bc8bb0f
Jason <1123150116@global.ac.id>
2026-03-05 23:45:32 +0700

    Add cart repository implementation

---
+++ lib/features/cart/domain/repositories/cart_repository_impl.dart
@@ -0,0 +1,26 @@
+import '../../domain/entities/product.dart';
+import '../../domain/repositories/cart_repository.dart';
+
+class CartRepositoryImpl implements CartRepository {
+  final List<Product> _items = [];
+
+  @override
+  List<Product> getCartItems() {
+    return List.unmodifiable(_items);
+  }
+
+  @override
+  void addItem(Product product) {
+    _items.add(product);
+  }
+
+  @override
+  void removeAllItems() {
+    _items.clear();
+  }
+
+  @override
+  bool isItemInCart(String productId) {
+    return _items.any((item) => item.id == productId);
+  }
+}
