bbee33fdf2bc6d066e92f4bf96c31418decaa10b
Jason <1123150116@global.ac.id>
2026-03-05 23:40:57 +0700

    Add product entity and cart repository interface

---
+++ lib/features/cart/domain/repositories/cart_repository.dart
@@ -0,0 +1,11 @@
+import '../entities/product.dart';
+
+abstract class CartRepository {
+  List<Product> getCartItems();
+
+  void addItem(Product product);
+
+  void removeAllItems();
+
+  bool isItemInCart(String productId);
+}
