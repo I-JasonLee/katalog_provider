790f33681117f729ab570f44ac709abbb687ed41
Jason <1123150116@global.ac.id>
2026-03-05 23:58:00 +0700

    Add app router

---
+++ lib/core/routes/app_router.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import '../../features/cart/presentation/pages/catalog_page.dart';
+import '../../features/cart/presentation/pages/cart_page.dart';
+
+class AppRouter {
+  static const String catalog = '/';
+  static const String cart = '/cart';
+
+  static Map<String, WidgetBuilder> get routes {
+    return {
+      catalog: (context) => const CatalogPage(),
+      cart: (context) => const CartPage(),
+    };
+  }
+}
